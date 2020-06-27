2ef72967f1318fbd7665dc52115691ce7a79692d
edwin <edwin.ivan.torres@gmail.com>
2020-06-26 21:58:00 -0500

    Updates

---
+++ precache-manifest.db2919f9133f2d02707eeb44be467b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abca3de20f121317872d95e891a0f14d",
+    "url": "/github-page/index.html"
+  },
+  {
+    "revision": "19171e6a0d67a3b455dc",
+    "url": "/github-page/static/css/main.0da0c008.chunk.css"
+  },
+  {
+    "revision": "7ae02b7ac306a5f598a9",
+    "url": "/github-page/static/js/2.aae9104c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-page/static/js/2.aae9104c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19171e6a0d67a3b455dc",
+    "url": "/github-page/static/js/main.38f3a005.chunk.js"
+  },
+  {
+    "revision": "9b55972bb59066d8fb32",
+    "url": "/github-page/static/js/runtime-main.d6abdcb6.js"
+  }
+]);
