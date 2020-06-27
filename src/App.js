f397e54e4bf7a49e0e68c170f16a134315b2f263
edwin <edwin.ivan.torres@gmail.com>
2020-06-26 21:56:52 -0500

    deploy react app to githup pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router >
+    <div className = "App">
+              <Nav />
+              <Switch>
+                    <Route path = "/" exact component = {Home} />
+                  
+                  
+                   
+              </Switch>
     </div>
+</Router>
   );
 }
+
+
+
+const Home = () => (
+  <div>
+    <div style = {{marginTop:"1.5%"}}>
+   
+    <p>My names is Edwin.  I'm a <em>Dreamer</em> learning to code. </p>
+    <p>I also teach math at Laredo College. </p>
+
+    </div>
+  
+    </div>
+
+);
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,63 @@
+import React from 'react';
+ 
+import './App.css';
+
+import {Link} from 'react-router-dom';
+
+function Nav() {
+        const navStyle = {
+                    color: 'white'
+        };
+
+
+  return (
+    <nav>
+        <h3>LOGO</h3>
+     
+            <ul className = "nav-links">
+            <Link style = {navStyle} to = "/">
+                            <li >Home</li>
+                    </Link>
+  
+            </ul>
+
+    </nav>
+  );
+}
+
+export default Nav;
+
+
+export function ProjectNav() {
+        const navStyle = {
+                    color: 'white'
+        };
+
+
+  return (
+    <nav className = "projects-nav">
+        <h3>ProjectNav</h3>
+     
+            <ul className = "nav-links">
+            <Link style = {navStyle} to = "/">
+                            <li >Home</li>
+                    </Link>
+
+                    <Link style = {navStyle} to = "/projects/shop">
+                            <li >Shop</li>
+                    </Link>
+                    <Link style = {navStyle} to = "/pokemon">
+                            <li >Pokemon</li>
+                    </Link>
+                    <Link style = {navStyle} to = "/projects/breakout">
+                            <li >breakout</li>
+                    </Link>
+                    <Link style = {navStyle} to = "/projects">
+                            <li >Projects</li>
+                    </Link>
+            
+            </ul>
+
+    </nav>
+  );
+}
